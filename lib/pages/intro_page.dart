969c88304634a8605f9c3e9291488ac7821414ef
bogibekdev <bogibekdev@gmail.com>
2023-05-03 20:48:15 +0500

    project finished

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:introui/pages/home_page.dart';
+import 'package:introui/utils/Strings.dart';
 
 class IntroPage extends StatefulWidget {
   static const String id = "IntroPage";
+
   const IntroPage({Key? key}) : super(key: key);
 
   @override
@@ -9,8 +12,140 @@ class IntroPage extends StatefulWidget {
 }
 
 class _IntroPageState extends State<IntroPage> {
+  late PageController _pageController;
+  int currentPage = 0;
+
+  @override
+  void initState() {
+    _pageController = PageController(initialPage: 0);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        elevation: 0,
+        actions: [
+          GestureDetector(
+            onTap: () {
+              Navigator.pushReplacementNamed(context, HomePage.id);
+            },
+            child: const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Text(
+                "Skip",
+                style: TextStyle(color: Colors.green, fontSize: 16),
+              ),
+            ),
+          )
+        ],
+      ),
+      body: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          PageView(
+            onPageChanged: (int page) {
+              setState(() {
+                currentPage = page;
+              });
+            },
+            controller: _pageController,
+            children: [
+              makePage(
+                image: "assets/images/img.png",
+                title: Strings.stepOneTitle,
+                content: Strings.stepOneContent,
+              ),
+              makePage(
+                image: "assets/images/img_1.png",
+                title: Strings.stepSecondTitle,
+                content: Strings.stepSecondContent,
+              ),
+              makePage(
+                image: "assets/images/img_2.png",
+                title: Strings.stepThirdTitle,
+                content: Strings.stepThirdContent,
+              ),
+            ],
+          ),
+          Container(
+            margin: const EdgeInsets.only(bottom: 40),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: _buildIndicator(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget makePage({image, title, content}) {
+    return Container(
+      padding: const EdgeInsets.only(
+        left: 50,
+        right: 50,
+        bottom: 50,
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Image.asset(image),
+              ),
+              Text(
+                title,
+                style: const TextStyle(color: Colors.green, fontSize: 20),
+              ),
+              const SizedBox(height: 30),
+              Text(
+                content,
+                style: const TextStyle(
+                  color: Colors.grey,
+                  fontSize: 15,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _indicator(bool isActive) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 300),
+      height: 6,
+      width: isActive ? 30 : 6,
+      margin: const EdgeInsets.only(right: 5),
+      decoration: BoxDecoration(
+          color: Colors.green, borderRadius: BorderRadius.circular(5)),
+    );
+  }
+
+  List<Widget> _buildIndicator() {
+    List<Widget> indicators = [];
+    for (int i = 0; i < 3; i++) {
+      if (i == currentPage) {
+        indicators.add(_indicator(true));
+      } else {
+        indicators.add(_indicator(false));
+      }
+    }
+
+    return indicators;
   }
 }
