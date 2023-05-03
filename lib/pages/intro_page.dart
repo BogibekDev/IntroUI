e8b0f920adba8e07072ecd22ba0c6544f6f68bf5
bogibekdev <bogibekdev@gmail.com>
2023-05-03 15:34:57 +0500

    clean project and add some page and Strings

---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class IntroPage extends StatefulWidget {
+  static const String id = "IntroPage";
+  const IntroPage({Key? key}) : super(key: key);
+
+  @override
+  State<IntroPage> createState() => _IntroPageState();
+}
+
+class _IntroPageState extends State<IntroPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
